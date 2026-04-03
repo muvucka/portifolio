a50388b081c4417f26d96ae2ef6f9daf21908713
Wilson <pw.druida@gmail.com>
2026-04-03 03:45:05 -0300

    updated back port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,10 @@ import app from "./app.js";
 import { syncScryfallSets } from "./services/scryfallSync.js";
 import cron from "node-cron";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
-  console.log(`Server rodando em http://localhost:${PORT}`);
+  console.log(`Server rodando em http://localhost:${PORT} ou na porta Railway ${PORT}`);
 
   console.log("Rodando sync inicial...");
   
@@ -13,7 +13,6 @@ app.listen(PORT, async () => {
 
   console.log("Sync inicial concluído");
   
-  // 🔥 roda em background (não bloqueia o server)
   // roda a cada 6 horas
 cron.schedule("0 */6 * * *", () => {
   console.log("Rodando sync agendado...");
