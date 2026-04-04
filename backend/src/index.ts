30f189fc4f33c75614fffcae967a75ea523d1fc2
Wilson <pw.druida@gmail.com>
2026-04-04 01:39:08 -0300

    fix error PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import app from "./app.js";
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log("PORTA DO PROCESSO:", process.env.PORT);
 });
